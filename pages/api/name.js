fecec5abeaa6f0a167773939cc11818f6959a0cc
TAO (DAVID) SHAN <shan2000814@gmail.com>
2024-02-04 09:56:33 -0500

    new
    new

---
+++ pages/api/name.js
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+export default async function handler(req, res) {
+    const {name} = req.body
+    const openai = new OpenAI({
+      api_key: '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'
+    });
+
+    const textCompletion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "Please geneate name for the animal:"+name+". I want 5 names for the output." }],
+      model: "gpt-3.5-turbo",
+    })
+
+     // Extract the generated names
+    const generatedText = textCompletion.choices[0];
+
+    //  //Generate an image using DALL-E 3
+     const image = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: "Generate one sample picture for animal " + name, // Use the name received in the request
+        n: 1,
+        size: "1024x1024",
+      });
+ 
+     res.status(200).json({
+       text: generatedText,
+       image: image.data // This will be a URL to the generated image
+     });
+    //res.status(200).json(generatedText);
+
+}
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState , useEffect } from 'react';
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = async () => {
+    if (!inputValue) return; // Don't call API if input is empty
+    fetch('/api/name',{
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({"name": inputValue}), // body data type must match "Content-Type" header
+      })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data)
+            const {content} = data.message
+            console.log(content)
+            setOutput(content)
+          })
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Title</h1>
+      <input type="text" placeholder="Enter text here" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleClick}>Click Me</button>
+      {
+        output &&
+      <div>{output}</div>
+
+      }
+    </div>
+  );
+}
